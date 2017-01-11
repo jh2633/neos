b5f7fabe1a13b429052bef819a17b9385e8270a6
Joseph <joseph.jr@gmail.com>
2017-01-11 17:39:39 +0000

    add shopping cart controller

--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -1,22 +1,27 @@
 require './app/models/basket'
 
 describe Basket do
-
+  before(:each) do
+    item = double("item")
+  end
   subject(:basket) {described_class.new}
 
-  it 'can add and calculate the total of the basket items' do
-    item = double("item")
-    basket.add(item)
-    expect(basket.number_of_items).to eq 1
-  end
 
-  it 'can remove items from the basket' do
-    item = double("item")
-    basket.add(item)
-    expect(basket.number_of_items).to eq 1
-    basket.remove(item)
-    expect(basket.number_of_items).to eq 0
+  describe "adding and removing items from a basket" do
 
-  end
+      it 'can add and calculate the total of the basket items' do
+        basket.add(item)
+        expect(basket.number_of_items).to eq 1
+      end
+
+      it 'can remove items from the basket' do
+        item = double("item")
+        basket.add(item)
+        expect(basket.number_of_items).to eq 1
+        basket.remove(item)
+        expect(basket.number_of_items).to eq 0
+      end
+
+    end
 
 end
